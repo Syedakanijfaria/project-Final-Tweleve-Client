c26076259d0f84a0709c5c7afc1c4ab52fde55ce
SKFaria <fariafaria737@gmail.com>
2021-11-15 21:25:54 +0600

    final

--- src/Pages/Home/ExtraSection/ExtraSection.js
+++ src/Pages/Home/ExtraSection/ExtraSection.js
@@ -2,27 +2,29 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import doctor from '../../../images/doctor.jpg'
-// import bg from '../../../images/appointment-bg.png'
 import { Button, Typography } from '@mui/material';
 
-const appointmentBanner = {
-    background: `url(${doctor})`,
-    backgroundColor: 'rgba(45, 58, 74, 0.9)',
-    backgroundBlendMode: 'darken, luminosity',
-    marginTop: 175
-}
+// const appointmentBanner = {
+//     background: `url(${doctor})`,
+//     backgroundColor: 'rgba(45, 58, 74, 0.9)',
+//     backgroundBlendMode: 'darken, luminosity',
+//     marginTop: 175
+// }
 
 const ExtraSection = () => {
     return (
-        <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
+        <Box sx={{ flexGrow: 1, mt: 5, mb: 4 }}>
+            <Grid container spacing={2} sx={{ height: "500px" }}>
                 <Grid item xs={12} md={12} sx={{
                     display: 'flex',
                     justifyContent: 'flex-start',
                     alignItems: 'center',
                     textAlign: 'left'
                 }}>
-                    <Box>
+                    <Grid >
+                        <img sx={{ height: "500px" }} src={doctor} />
+                    </Grid>
+                    <Grid>
                         <Typography variant="h6" sx={{ mb: 5 }} style={{ color: '#5CE7ED' }}>
                             Appointment
                         </Typography>
@@ -32,8 +34,7 @@ const ExtraSection = () => {
                         <Typography variant="h6" sx={{ my: 5 }} style={{ color: 'white', fontSize: 14, fontWeight: 300 }}>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi facere totam aliquam praesentium vel. Amet veniam odio vero doloremque laborum.
                         </Typography>
-                        <Button variant="contained" style={{ backgroundColor: '#5CE7ED' }}>Learn More</Button>
-                    </Box>
+                    </Grid>
                 </Grid>
             </Grid>
         </Box>
